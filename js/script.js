9cbb97c6106b45fb2ed6c311ff087413f2ca1a9b
JuanLeonelC <juanleonelcd@gmail.com>
2022-06-05 00:16:40 -0300

    agregue funciones

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@ let menu = [{ id: 1, nombre: 'Pizza', precio: 20, stock: '3' },
 { id: 2, nombre: 'Pancho', precio: 3, stock: '7' },
 { id: 3, nombre: 'Hamburguesa', precio: 15, stock: '0' }];
 alert("Hola, bienvenido al restaurante");
-let opcion
+function CalcPrecio(id) {
+    if (menu[id].stock > 0) {
+        precio += menu[id].precio;
+        menu[id].stock--;
+        alert("su pedido de " + menu[id].nombre + " ha sido agregado al carrito");
+        return precio;
+    }
+    else{
+        alert("No hay mas stock");
+    }    
+
+}
 let precio = 0;
+let opcion
 do{
     let opcion = prompt('que desea hacer\n 1. Ver menu\n 2. pedir comida\n 3. retirar su comida\n 4. Salir');
     if (opcion == 1) {
@@ -17,32 +29,12 @@ do{
             menu[0].id + '- ' +menu[0].nombre + ' vale ' + menu[0].precio + ' tenemos ' + menu[0].stock + "\n" +
             menu[1].id + '- ' + menu[1].nombre + ' vale ' + menu[1].precio + ' tenemos ' + menu[1].stock + "\n" +
             menu[2].id + '- ' + menu[2].nombre + ' vale ' + menu[2].precio + ' tenemos ' + menu[2].stock);
-        if (pedir == 1) {
-            if (menu[0].stock > 0) {
-                alert('Su pedido es: ' + menu[0].nombre + ' vale ' + menu[0].precio);
-                menu[0].stock--;
-                precio += menu[0].precio;
-            } else {
-                alert('Lo sentimos, no hay stock de este producto');
-            }
+        if (pedir > 0 && pedir < 4) {
+            pedir -= 1;
+            CalcPrecio(pedir);
         }
-        if (pedir == 2) {
-            if (menu[1].stock > 0) {
-                alert('Su pedido es: ' + menu[1].nombre + ' vale ' + menu[1].precio);
-                menu[1].stock--;
-                precio += menu[1].precio;
-            } else {
-                alert('Lo sentimos, no hay stock de este producto');
-            }
-        }
-        if (pedir == 3) {
-            if (menu[2].stock > 0) {
-                alert('Su pedido es: ' + menu[2].nombre + ' vale ' + menu[2].precio);
-                menu[2].stock--;
-                precio += menu[2].precio;
-            } else {
-                alert('Lo sentimos, no hay stock de este producto');
-            }
+        else{
+            alert('No existe esa opcion');
         }
     }
     if (opcion == 3) {
