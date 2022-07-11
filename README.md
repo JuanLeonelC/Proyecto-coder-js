38124d76251dba8dc3085c8b3fc527db9348de3a
JuanLeonelC <juanleonelcd@gmail.com>
2022-07-11 17:01:00 -0300

    entrega final

---
+++ README.md
@@ -0,0 +1,11 @@
+# Proyecto Coderhouse JavaScript
+
+Este proyecto, es un juego clicker simple, creado en javascript en el cual el jugador, gana puntos a travez de un sistema de eventos, para poder comprar diversas mejoras para el juego.
+
+# Como esta creado
+
+Este juego clicker esta creado, a travez de eventos onclick, que ayudan al jugador a conseguir puntos al hacer click, con estos puntos se pueden comprar diversas mejoras, las cuales otorgan diversas ayudar a conseguir mas puntos o dar puntos cada cierto tiempo, estas mejoras estan guardadas en un archivo JSON y importadas. Dentro del archivo js a travez de fetch y una funcion async, para poder modificar sus valores para el sistema de guardado y de compra del juego.
+
+# Agregar contenido
+
+El juego esta creado con la idea de poder aumentar la cantidad de mejoras o de agregar algun sistema para recompenzar al jugador.
--- js/script.js
+++ js/script.js
@@ -10,21 +10,21 @@ let game = { // configuracion del juego
 // funciones del juego
 let upgrades = requestUpgrades();
 let display = {  // actualiza el display del juego
-    update() {
+    update() { // acutaliza el display de los puntos
         document.getElementById("score").innerHTML = game.score;
         document.getElementById("clickPower").innerHTML = "+" + game.clickPower;
         document.getElementById("scorePerSecond").innerHTML = "+" + game.scorePerSecond;
 },
-    updateBuys(index) {
+    updateBuys(index) { // actualiza el display al comprar mejoras
     document.getElementById("upgrade" + index).innerHTML = "<img src=" + upgrades[index].img + "><div><span class = 'titulo'>" + upgrades[index].name + "</span><span>vale " + upgrades[index].price + "</span><span>" + upgrades[index].description + "</span><span>tienes " + upgrades[index].quantity + "</span></div>";
 },
-    updateShop()  {
+    updateShop()  { // actualiza el display de todas las mejoras
         for (let i = 0; i < upgrades.length; i++) {
             document.getElementById("upgrade" + i).innerHTML = "<img src=" + upgrades[i].img + "><div><span class = 'titulo'>" + upgrades[i].name + "</span><span>vale " + upgrades[i].price + "</span><span>" + upgrades[i].description + "</span><span>tienes " + upgrades[i].quantity + "</span></div>";
         }
 }
 }
-async function requestUpgrades(){
+async function requestUpgrades(){ // importa las mejoras del archivo JSON
     const response = await fetch("js/module/upgrades.json");
     const data = await response.json();
     upgrades = data;
@@ -32,7 +32,7 @@ async function requestUpgrades(){
     return upgrades;
 }
 
-function createUpgrades(){ // crea los upgrades
+function createUpgrades(){ // crea las mejoras
     for (item in upgrades) {
         let newItem = document.createElement('div');
         document.createAttribute("id")
@@ -45,7 +45,7 @@ function createUpgrades(){ // crea los upgrades
     loadGame();
 }
 
-function buy(index) {  // maneja el sistema de buy upgrades
+function buy(index) {  // maneja el sistema de Compra
     if (game.score >= upgrades[index].price) {
         game.score -= upgrades[index].price;
         upgrades[index].quantity++;
@@ -54,20 +54,19 @@ function buy(index) {  // maneja el sistema de buy upgrades
         game.scorePerSecond += upgrades[index].upgradePerSecond;
         display.updateBuys(index);
         display.update();
-        console.log(upgrades[index]);
     }
 }
 
-function offlineTime() {
+function offlineTime() { // revisa cuanto tiempo estuvo desconectado el jugador
     let end = DateTime.now();
     start = DateTime.fromISO(start);
     var diffInSeconds = end.diff(start, 'seconds');
     diffInSeconds.toObject();
     difference = diffInSeconds.seconds
-    if (difference <= 120){
+    if (difference >= 120){ // si estuvo desconectado por mas de 2 minutos se suman puntos
         points = difference * game.scorePerSecond
         Math.round(points / 2)
-        if (points >= 1){
+        if (points >= 1){ // evitar que se sumen 0 puntos
             notification(Math.round(points / 2))
             game.score += Math.round(points / 2)
         }
@@ -75,7 +74,7 @@ function offlineTime() {
     
 }
 
-function notification(points) {
+function notification(points) { // lanza una notificacion anunciando los puntos que se sumaron
     Toastify({
         text: "ganaste " + points + " puntos mientras no estabas",
         style: {
@@ -111,8 +110,6 @@ function saveGame(){ // guarda la partida
 
 function loadGame(){ // carga la partida
     var load = JSON.parse(localStorage.getItem("game"));
-    console.log(load.upgrades);
-    console.log(upgrades);
     if(load != null){
         if (typeof load.score != "undefined") game.score = load.score;
         if (typeof load.clickPower != "undefined") game.clickPower = load.clickPower;
@@ -121,22 +118,22 @@ function loadGame(){ // carga la partida
         if (typeof load.upgrades != "undefined"){
             for(let i = 0; i < upgrades.length; i++){
                 if(typeof load.upgrades[i].quantity != "undefined") upgrades[i].quantity = load.upgrades[i].quantity;
-                console.log(upgrades[i].quantity);
                 if(typeof load.upgrades[i].price != "undefined") upgrades[i].price = load.upgrades[i].price;
     }}}
     offlineTime()
     display.updateShop();
 }
+
 setInterval(function() { // suma los puntos por segundo a la puntuacion
     game.score += game.scorePerSecond;
     display.update();
 }, 1000);
 
-setInterval(function() { // guarda la partida cada 30
+setInterval(function() { // guarda la partida cada 30 segundos
     saveGame()
 }, 30000);
 
-window.onload = function() { // carga la partida
+window.onload = function() { // actualiza el display al cargar la pagina
     display.update();
 }
 // interfaz del juego
@@ -147,16 +144,16 @@ button.addEventListener("click", function() {
 }
 );
 
-let menu = document.getElementById("menu"); // menu de upgrades
+let menu = document.getElementById("menu"); // menu de mejoras
 
 
-let saveButton = document.getElementById("game"); // button para saveButton la partida
+let saveButton = document.getElementById("game"); // boton para guardar la partida
 saveButton.addEventListener("click", function() {
     saveGame();
 }
 );
 
-let restartButton = document.getElementById("restart"); // button para restart el juego
+let restartButton = document.getElementById("restart"); // boton para reiniciar la partida
 restartButton.addEventListener("click", function() {
     restart();
 }
